b1a6f71c15c778f0d0e8929caf56674b20ecf981
renzz1762 <rhmnpx356356@gmail.com>
2026-05-26 21:13:45 +0800

    Clear MAINTENANCE_TEXT for maintenance mode

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ const CFG = {
    *
    * MATIIN MAINTENANCE: kosongkan MAINTENANCE_TEXT
    */
-  MAINTENANCE_TEXT: "TUNGGU FIX BUG SEKITAR 100 MILIAR JAM",
+  MAINTENANCE_TEXT: "",
   MAINTENANCE_MODE: "manual",       // "auto" = dengan timer | "manual" = sampai dimatiin manual
   MAINTENANCE_DURATION_HOURS: 2,
   MAINTENANCE_START: "",
